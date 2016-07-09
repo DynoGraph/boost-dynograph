564c9a06fa3d786bc37aaa1ab09f88e83808515e
Eric Hein <ehein6@gatech.edu>
2016-07-09 13:55:46 -0400

    Implement dynograph batch loading.

--- main.cc
+++ main.cc
@@ -11,41 +11,37 @@
 #include <fstream>
 #include <chrono>
 
-extern "C" {
-    #include <dynograph_util.h>
-}
+#include <dynograph_util.hh>
 
-using std::cout;
 using std::cerr;
-using std::endl;
+using std::cerr;
 using std::ifstream;
 using std::map;
 using std::chrono::steady_clock;
 using std::chrono::duration;
+using std::chrono::steady_clock;
+using std::string;
 
 namespace po = boost::program_options;
 
 typedef boost::vecS OutEdgeList;
 typedef boost::vecS VertexList;
-
+// TODO add graph properties
 typedef boost::adjacency_list<OutEdgeList, VertexList, boost::bidirectionalS> Graph;
+typedef boost::graph_traits<Graph>::vertex_descriptor VertexId;
 
-namespace std {
-    template<typename T>
-    std::istream &operator>>(std::istream &in, std::pair<T, T> &p) {
-        in >> p.first >> p.second;
-        return in;
-    }
+void printTime(string stepDesc, duration<double, std::milli> diff)
+{
+    cerr << stepDesc << ": " << diff.count() << " ms.\n";
 }
 
 int main(int argc, char *argv[]) {
-
-    std::string inputGraph;
-
+    // Parse the command line
+    std::string inputPath;
     po::options_description desc("Usage: ");
     desc.add_options()
         ("help", "Display help")
-        ("input", po::value<std::string>(&inputGraph), "Path for the graph to load")
+        ("input", po::value<std::string>(&inputPath), "Path for the graph to load")
     ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -56,44 +52,51 @@ int main(int argc, char *argv[]) {
         cerr << desc << "\n";
         return -1;
     }
+  
+    // Create the graph
+    // TODO scale this up with available system memory
+    VertexId max_num_vertices = 10001;
+    Graph g(max_num_vertices);
 
-    cerr << "Loading " << inputGraph << "\n";
-    std::ifstream input(inputGraph);
+    // Pre-load the edge batches
+    cerr << "Pre-loading " << inputPath << " from disk...\n";
+    auto t1 = steady_clock::now();
 
-    typedef boost::graph_traits<Graph>::vertices_size_type size_type;
-    typedef boost::graph_traits<Graph>::vertex_descriptor vertex;
+    DynoGraph::Dataset dataset(inputPath, 20);
 
-    std::map<vertex, double> mymap;
-    boost::associative_property_map<std::map<vertex, double>> rank_map(mymap);
+    auto t2 = steady_clock::now();
+    printTime("Graph pre-load", t2 - t1);
 
-    size_type n_vertices = 10001;
+    // Ingest each batch and run analytics
+    for (int batchId = 0; batchId < dataset.getNumBatches(); ++batchId)
+    {
+        cerr << "Loading batch " << batchId < "...\n";
+        t1 = steady_clock::now();
 
-    std::istream_iterator<std::pair<size_type, size_type>> input_begin(input), input_end;
+        // Batch insertion
+        for (DynoGraph::Edge e : dataset.getBatch(batchId))
+        {   
+            // TODO check return value and update weight&timestamp if present
+            add_edge(e.src, e.dst, g);
+        }
 
-    auto start = steady_clock::now();
+        t2 = steady_clock::now();
+        printTime("Edge stream", t2 - t1);
 
-    Graph g(input_begin, input_end, n_vertices);
+        cerr << "Number of vertices: " << g.m_vertices.size() << "\n";
+        cerr << "Number of edges: " << g.m_edges.size() << "\n";
 
-    auto end = steady_clock::now();
+        cerr << "Running page rank...\n";
+        t1 = steady_clock::now();
 
-    auto diff = end - start;
+        // Algorithm
+        std::map<VertexId, double> mymap;
+        boost::associative_property_map<std::map<VertexId, double>> rank_map(mymap);
+        boost::graph::page_rank(g,  rank_map, boost::graph::n_iterations(20), 0.85);
 
-    cout << "Import done in " << duration<double, std::milli>(diff).count() << " milliseconds. " << endl;
-
-    cout << "Number of vertices: " << g.m_vertices.size() << endl;
-    cout << "Number of edges: " << g.m_edges.size() << endl;
-
-    cout << "Running page rank." << endl;
-
-    start = steady_clock::now();
-
-    boost::graph::page_rank(g,  rank_map, boost::graph::n_iterations(20), 0.85);
-
-    end = steady_clock::now();
-
-    diff = end - start;
-
-    cout << "Page rank done in " << duration<double, std::milli>(diff).count() << " milliseconds. " << endl;
+        t2 = steady_clock::now();
+        printTime("Page rank", t2 - t1);
+    }
 
     return 0;
 }

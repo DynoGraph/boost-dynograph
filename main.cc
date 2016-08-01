abe0706d09571d342a7a4c9496b489855548e64c
Eric Hein <ehein6@gatech.edu>
2016-08-01 19:39:55 -0400

    Got a few parallel algorithms working; incremental commit.

--- main.cc
+++ main.cc
@@ -5,12 +5,10 @@
 #include <fstream>
 #include <chrono>
 
-#include <boost/graph/edge_list.hpp>
-#include <boost/graph/directed_graph.hpp>
-#include <boost/graph/page_rank.hpp>
-#include <boost/graph/betweenness_centrality.hpp>
-#include <boost/graph/connected_components.hpp>
-#include <boost/graph/clustering_coefficient.hpp>
+#include <boost/graph/distributed/page_rank.hpp>
+#include <boost/graph/distributed/betweenness_centrality.hpp>
+#include <boost/graph/distributed/connected_components_parallel_search.hpp>
+
 #include <boost/graph/parallel/basic_reduce.hpp>
 #include <boost/property_map/property_map.hpp>
 #include <boost/property_map/parallel/distributed_property_map.hpp>
@@ -19,12 +17,10 @@
 #include <hooks.h>
 
 using std::cerr;
-using std::cerr;
 using std::ifstream;
 using std::map;
 using std::chrono::steady_clock;
 using std::chrono::duration;
-using std::chrono::steady_clock;
 using std::string;
 
 void printTime(string stepDesc, duration<double, std::milli> diff)
@@ -73,31 +69,37 @@ getArgs(int argc, char **argv)
 
 void insertBatch(DynoGraph::Batch batch, Graph &g)
 {
+    // TODO Parallel graph load
     for (DynoGraph::Edge e : batch)
     {
-        // Try to insert the edge
-        std::pair<Edge, bool> inserted_edge = boost::add_edge(
-            boost::vertex(e.src, g),
-            boost::vertex(e.dst, g),
-            // Boost uses template nesting to implement multiple edge properties
-            Weight(e.weight, Timestamp(e.timestamp)),
-            g);
-        // If the edge already existed...
-        if (!inserted_edge.second)
-        {
-            Edge &existing_edge = inserted_edge.first;
-            // Increment edge weight
-            int64_t weight = get(boost::edge_weight, g, existing_edge);
-            weight += e.weight;
-            put(boost::edge_weight, g, existing_edge, weight);
-            // Overwrite timestamp
-            put(boost::edge_timestamp, g, existing_edge, e.timestamp);
-        }
+
+        VertexId Src = boost::vertex(e.src, g);
+        VertexId Dst = boost::vertex(e.dst, g);
+        // Only insert edge if this process owns the source vertex
+        //if (Src.owner == process_id(g.process_group())) {
+            // Try to insert the edge
+            std::pair<Edge, bool> inserted_edge = boost::add_edge(
+                    Src, Dst,
+                    // Boost uses template nesting to implement multiple edge properties
+                    Weight(e.weight, Timestamp(e.timestamp)),
+                    g);
+            // If the edge already existed...
+            if (!inserted_edge.second) {
+                Edge &existing_edge = inserted_edge.first;
+                // Increment edge weight
+                int64_t weight = get(boost::edge_weight, g, existing_edge);
+                weight += e.weight;
+                put(boost::edge_weight, g, existing_edge, weight);
+                // Overwrite timestamp
+                put(boost::edge_timestamp, g, existing_edge, e.timestamp);
+            }
+        //}
     }
 }
 
 void deleteEdges(int64_t threshold, Graph &g)
 {
+    // TODO Make sure this is actually running in parallel
     boost::remove_edge_if(
         [&](const Edge &e)
         {
@@ -107,7 +109,7 @@ void deleteEdges(int64_t threshold, Graph &g)
 }
 
 //std::vector<VertexId> bfsRoots = {3, 30, 300, 4, 40, 400};
-std::vector<string> algs = {"bfs", "bc", "cc", "clustering", "pagerank"};
+std::vector<string> algs = {"all", "bc", "cc", "pagerank"};
 
 void runAlgorithm(string algName, Graph &g, int64_t trial)
 {
@@ -120,48 +122,46 @@ void runAlgorithm(string algName, Graph &g, int64_t trial)
             runAlgorithm(alg, g, trial);
         }
     }
-/*
-    else if (algName == "bfs")
-    {
-        for (VertexId root : bfsRoots)
-        {
-            boost::graph::breadth_first_search(g, root);
-        }
-    }
 
     else if (algName == "bc")
     {
-        typedef boost::property_map<Graph, boost::vertex_index_t>::const_type IndexMap;
-        typedef boost::iterator_property_map<std::vector<int>::iterator, IndexMap> CentralityMap;
-
-        std::vector<int> centralityS(boost::num_vertices(g), 0);
-        CentralityMap centrality(centralityS.begin(), get(boost::vertex_index, g));
-
-        boost::brandes_betweenness_centrality(g, centrality);
+        std::vector<int64_t> local_centrality_vec(boost::num_vertices(g), 0);
+        boost::brandes_betweenness_centrality(
+            g,
+            make_iterator_property_map(local_centrality_vec.begin(), get(boost::vertex_index, g))
+        );
     }
-*/
+
     else if (algName == "cc")
     {
-        // FIXME use incremental_components here instead
+        // FIXME use incremental_components here instead (st_components)
         std::vector<int> local_components_vec(boost::num_vertices(g));
-        typedef boost::iterator_property_map<std::vector<int>::iterator, boost::property_map<Graph, boost::vertex_index_t>::type> ComponentMap;
-        ComponentMap component(local_components_vec.begin(), get(boost::vertex_index, g));
-    }
-/*
-    else if (algName == "clustering")
-    {
-        std::map<VertexId, int64_t> vertexCoefficients;
-        boost::associative_property_map<std::map<VertexId, int64_t>> coefficients_map(vertexCoefficients);
-        boost::all_clustering_coefficients(g, coefficients_map);
+        //typedef boost::iterator_property_map<std::vector<int>::iterator, boost::property_map<Graph, boost::vertex_index_t>::type> ComponentMap;
+        //ComponentMap component(local_components_vec.begin(), get(boost::vertex_index, g));
+        //boost::graph::distributed::connected_components_ps(g, component);
+        boost::graph::distributed::connected_components_ps(
+            g,
+            boost::parallel::make_iterator_property_map(local_components_vec.begin(), get(boost::vertex_index, g))
+        );
+
     }
 
     else if (algName == "pagerank")
     {
-        std::map<VertexId, double> vertexRanks;
-        boost::associative_property_map<std::map<VertexId, double>> rank_map(vertexRanks);
-        boost::graph::page_rank(g, rank_map, boost::graph::n_iterations(20), 0.85);
+        std::vector<double> vertexRanks(num_vertices(g));
+        std::vector<double> vertexRanks2(num_vertices(g));
+
+        boost::graph::distributed::page_rank(
+            g,
+            make_iterator_property_map(vertexRanks.begin(), get(boost::vertex_index, g)),
+            boost::graph::n_iterations(20),
+            0.85,
+            boost::num_vertices(g),
+            make_iterator_property_map(vertexRanks2.begin(), get(boost::vertex_index, g))
+        );
+
     }
-*/
+
     else
     {
         cerr << "Algorithm " << algName << " not implemented!\n";
@@ -169,49 +169,69 @@ void runAlgorithm(string algName, Graph &g, int64_t trial)
     }
 }
 
+#define MASTER_PROCESS (boost::graph::distributed::process_id(g.process_group()) == 0)
 
 int main(int argc, char *argv[]) {
+    // Initialize MPI
     boost::mpi::environment env(argc, argv);
+    ProcessGroup pg;
+
     // Parse the command line
     Args args = getArgs(argc, argv);
 
     // Create the graph
     // TODO scale this up with available system memory
-    Graph::vertices_size_type max_num_vertices = 10001;
-    Graph g(max_num_vertices);
-    
-    // Pre-load the edge batches
-    cerr << "Pre-loading " << args.inputPath << " from disk...\n";
-    auto t1 = steady_clock::now();
+    Graph::vertices_size_type max_num_vertices = 100001;
+    Graph g(max_num_vertices, pg);
 
-    DynoGraph::Dataset dataset(args.inputPath, args.numBatches);
+    DynoGraph::Dataset *dataset;
 
-    auto t2 = steady_clock::now();
-    printTime("Graph pre-load", t2 - t1);
+    // Master process pre-loads the batches
+    if (MASTER_PROCESS)
+    {
+        // Pre-load the edge batches
+        cerr << "Pre-loading " << args.inputPath << " from disk...\n";
+        auto t1 = steady_clock::now();
+
+        dataset = new DynoGraph::Dataset(args.inputPath, args.numBatches);
+
+        auto t2 = steady_clock::now();
+        printTime("Graph pre-load", t2 - t1);
+
+    // Other processes just wait for information
+    }
+    synchronize(pg);
 
     for (int64_t trial = 0; trial < args.numTrials; ++trial)
     {
         // Ingest each batch and run analytics
-        for (int batchId = 0; batchId < dataset.getNumBatches(); ++batchId)
+        for (int batchId = 0; batchId < args.numBatches; ++batchId)
         {
             // Deletions
-            int64_t modified_after = dataset.getTimestampForWindow(batchId, args.windowSize);
             if (args.enableDeletions)
             {
-                cerr << "Deleting edges older than " << modified_after << "\n";
-                Hooks::getInstance().region_begin("deletions", trial);
-                deleteEdges(modified_after, g);
-                Hooks::getInstance().region_end("deletions", trial);
+                if (MASTER_PROCESS)
+                {
+                    int64_t modified_after = dataset->getTimestampForWindow(batchId, args.windowSize);
+                    cerr << "Deleting edges older than " << modified_after << "\n";
+                    Hooks::getInstance().region_begin("deletions", trial);
+                    deleteEdges(modified_after, g);
+                    Hooks::getInstance().region_end("deletions", trial);
+                }
+                synchronize(pg);
             }
 
             // Batch insertion
-            cerr << "Loading batch " << batchId << "...\n";
-            Hooks::getInstance().region_begin("insertions", trial);
-            insertBatch(dataset.getBatch(batchId), g);
-            Hooks::getInstance().region_end("insertions", trial);
-
-            //cerr << "Number of vertices: " << g.m_vertices.size() << "\n";
-            //cerr << "Number of edges: " << g.m_edges.size() << "\n";
+            if (MASTER_PROCESS)
+            {
+                cerr << "Loading batch " << batchId << "...\n";
+                Hooks::getInstance().region_begin("insertions", trial);
+                insertBatch(dataset->getBatch(batchId), g);
+                Hooks::getInstance().region_end("insertions", trial);
+            }
+            synchronize(pg);
+            cerr << "P" << process_id(pg) << ": Number of vertices: " << num_vertices(g) << "\n";
+            cerr << "P" << process_id(pg) << ": Number of edges: " << num_edges(g) << "\n";
 
             // Algorithm
             Hooks::getInstance().region_begin(args.algName, trial);
@@ -220,6 +240,7 @@ int main(int argc, char *argv[]) {
 
         }
     }
+    if (MASTER_PROCESS) { delete dataset; }
 
     return 0;
 }

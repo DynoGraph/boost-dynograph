2f8b87a85bd2088945f203a2a46bd581dcc151df
Eric Hein <ehein6@gatech.edu>
2016-10-03 13:51:23 -0400

    Use distributed property map and explicit visitor to fix bfs crashing.

--- boost_algs.cpp
+++ boost_algs.cpp
@@ -36,6 +36,25 @@ pickSource(Graph &g, Graph::vertices_size_type maxNumVertices)
     return source;
 }
 
+template<typename DistanceMap>
+struct bfs_discovery_visitor : boost::bfs_visitor<>
+{
+    bfs_discovery_visitor(DistanceMap distance) : distance(distance)
+    {
+        boost::set_property_map_role(boost::vertex_distance, distance);
+    }
+
+    template<typename Edge, typename Graph>
+    void tree_edge(Edge e, const Graph& g)
+    {
+        std::size_t new_distance = get(distance, boost::source(e, g)) + 1;
+        put(distance, boost::target(e, g), new_distance);
+    }
+
+private:
+    DistanceMap distance;
+};
+
 void runAlgorithm(string algName, Graph &g, Graph::vertices_size_type maxNumVertices, int64_t trial)
 {
     if (g.process_group().rank == 0) { cerr << "Running " << algName << "...\n"; }
@@ -52,7 +71,14 @@ void runAlgorithm(string algName, Graph &g, Graph::vertices_size_type maxNumVert
     else if (algName == "bfs")
     {
         VertexId source = pickSource(g, maxNumVertices);
-        boost::graph::breadth_first_search(g, source);
+        std::vector<int> local_distance_vec(boost::num_vertices(g));
+        auto distance_map = make_iterator_property_map(local_distance_vec.begin(), get(boost::vertex_index, g));
+        bfs_discovery_visitor<decltype(distance_map)> visitor(distance_map);
+        breadth_first_search(
+            g,
+            source,
+            boost::visitor(visitor)
+        );
     }
 
     else if (algName == "cc")
